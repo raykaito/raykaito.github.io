7ae362081550ef973f2c8e1e45e517f270819412
raykaito <kazukykaito@gmail.com>
2019-09-06 15:32:51 +0900

    fixed an issue

--- 13_live_vision_system/numberScanner.js
+++ 13_live_vision_system/numberScanner.js
@@ -116,7 +116,7 @@ class VisionProgram_numberReader{
 				}
 				this.tryNumber(candidateLocal,mct.getImageData(0, this.numberCounter*17,16,16),this.fontsList[fontLocal],1);
 				this.checkTime();
-				if(this.timeIsUp) this.saveEverything(error,candidate,font+1,i,j);
+				if(this.timeIsUp) this.saveEverything(error,candidate,font,i,j+1);
 				if(j==this.fontsList.length-1) this.lastFont = 0;
 				if(this.timeIsUp) return;
 			}
