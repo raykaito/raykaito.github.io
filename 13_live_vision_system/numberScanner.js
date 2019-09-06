c9b04ef4c7c0ec24c27c1f5c9b4e337425f167e7
raykaito <kazukykaito@gmail.com>
2019-09-06 15:44:19 +0900

    fixed the stuck issue no3

--- 13_live_vision_system/numberScanner.js
+++ 13_live_vision_system/numberScanner.js
@@ -78,6 +78,7 @@ class VisionProgram_numberReader{
 				this.lastCell = i;
 				this.read(i%9,Math.floor(i/9));
 			}
+			this.checkTime();
 			if(this.timeIsUp) return;
 		}
 	}
@@ -115,12 +116,8 @@ class VisionProgram_numberReader{
 					fontLocal = j;
 				}
 				this.tryNumber(candidateLocal,mct.getImageData(0, this.numberCounter*17,16,16),this.fontsList[fontLocal],1);
-				this.checkTime();
+				if(j!=this.fontsList.length-1) this.checkTime();
 				if(this.timeIsUp) this.saveEverything(error,candidate,font,i,j+1);
-				if(j==this.fontsList.length-1){
-					this.lastFont = 0;
-					this.lastNumber++;
-				}
 				if(this.timeIsUp) return;
 			}
 			if(i==9) this.lastNumber = 1;
