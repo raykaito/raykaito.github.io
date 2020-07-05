6cd1ce508d01d41bdb6687e16ef3f072716fd173
raykaito <kazukykaito@gmail.com>
2020-07-05 23:28:07 +0900

    de

--- 16_madelbrot_set/canvasC.js
+++ 16_madelbrot_set/canvasC.js
@@ -1,4 +1,4 @@
-alert("H");
+alert("I");
 class Canvas{
 constructor(canvas=false,dim=false){
     //Initialize
@@ -28,6 +28,64 @@ getModXY(x,y){
     const xMod = this.center[0]+(x/this.canvas.width-1/2)*this.sideLength;
     const yMod = this.center[1]+(y/this.canvas.width-1/2)*this.sideLength;
     return [xMod,yMod];
+}
+resetPixels(){
+    for(let i=0;i<this.pixels.length;i++){
+        const [x,y] = this.i2xy(i);
+        const [xMod,yMod] = this.getModXY(x,y);
+        this.pixels[i] = new Pixel(xMod,yMod);
+    }
+}
+resize(dim){
+    if(dim.length!=2){
+        alert("Invalid dim dimension");
+        return;
+    }
+    this.pixelRatio = window.devicePixelRatio;
+    this.canvas.style.width  = dim[0] + "px";
+    this.canvas.style.height = dim[1] + "px";
+    this.canvas.width  = dim[0];
+    this.canvas.height = dim[1];
+    this.canvas.width *= this.pixelRatio;
+    this.canvas.height*= this.pixelRatio;
+}
+fill(){
+    this.ct.putImageData(this.imageData,0,0);
+}
+start(){
+    if(!this.loop) return;
+    this.counter++; 
+    let newDead = false;
+    let unWhiteFound = false;
+    for(let i=0;i<this.pixels.length;i++){
+        const result = this.pixels[i].update();
+        let value = 255;
+        if(result != false){
+            unWhiteFound = true;
+            if(result==this.counter) newDead = true;
+            value = result*255/this.counter;
+        }
+        this.setPix(i,value);
+    }
+    if(unWhiteFound&&newDead==false) this.loop=false;
+    this.fill();
+    text1.textContent = "Number of iterations: "+this.counter;
+    requestAnimationFrame(()=>{this.start();});
+}
+i2xy(i){
+    return [i%this.canvas.width,Math.floor(i/this.canvas.width)];
+}
+setPix(indexIn, value, type="all"){
+    //Check if indexIn is xy or index
+    let index;
+    if(indexIn.length==2)   index = this.xy2i(indexIn);
+    else                    index = indexIn;
+
+    //Set the pixel based on type
+    if(type=="all"||type==0) this.imageData.data[4*index+0] = value;
+    if(type=="all"||type==1) this.imageData.data[4*index+1] = value;
+    if(type=="all"||type==2) this.imageData.data[4*index+2] = value;
+    if(type=="all"||type==2) this.imageData.data[4*index+3] = 255;
 }
 }
 
