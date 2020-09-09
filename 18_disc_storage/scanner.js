4ca2713ce76c0f9732b82574048aac3719f7919c
raykaito <kazukykaito@gmail.com>
2020-09-09 14:28:30 +0900

    debug scanner

--- 18_disc_storage/scanner.js
+++ 18_disc_storage/scanner.js
@@ -66,6 +66,7 @@ class DiscScanner{
     drawVideo(){
         //adjust video size for display
         this.displayct.drawImage(this.video,0,this.croppedVideoYStart,this.videoWidth,this.croppedVideoHeight,0,0,this.displayCanvasWidth,this.displayCanvasHeight);
+        alert(this.video+","+0+","+this.croppedVideoYStart+","+this.videoWidth+","+this.croppedVideoHeight+","+0+","+0+","+this.displayCanvasWidth+","+this.displayCanvasHeight)
         requestAnimationFrame(()=>{this.drawVideo();});
     }
 }
