2d946ae90a4cb938dbbced29141a6406b26553c4
raykaito <kazukykaito@gmail.com>
2020-09-15 21:14:57 +0900

    interpretation of this is still dificult

--- 18_disc_storage/load_tv_programs.js
+++ 18_disc_storage/load_tv_programs.js
@@ -7,7 +7,7 @@ class TVListManager{
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200){
-            this.loadTVPrograms(this.responseText);
+            tvManager.loadTVPrograms(this.responseText);
             }
         };
         xhttp.open("GET", url, true);
