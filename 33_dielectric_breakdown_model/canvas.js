4321cc0641222d9abc5ce60d131424f0ac5cd911
raykaito <kazukykaito@gmail.com>
2021-09-04 14:10:30 +0900

    DBM implemented

--- 33_dielectric_breakdown_model/canvas.js
+++ 33_dielectric_breakdown_model/canvas.js
@@ -4,26 +4,45 @@ constructor(canvas){
     this.canvas = canvas;
     this.ct = canvas.getContext("2d");
     this.resizeCanvas();
+    this.resetCanvas();
 
-    //PotentialField
+    //Initialize potential and pixStatus Map
     this.potentialMap = new Array(this.canvas.width); // potential value [0,1]
+    this.pixStatusMap = new Array(this.canvas.width); // 0: uninvaded, 1: boundary, 2: path
+    this.parentMap = new Array(this.canvas.width);
     for(let x = 0; x < this.canvas.width; x++){
         this.potentialMap[x] = new Array(this.canvas.height);
-    }
-    for(let x = 0; x < this.canvas.width; x++){
+        this.pixStatusMap[x]  = new Array(this.canvas.height);
+        this.parentMap[x] = new Array(this.canvas.height);
         for(let y = 0; y < this.canvas.height; y++){
             this.potentialMap[x][y] = 0.5;
+            this.pixStatusMap[x][y] = 0;
+            this.parentMap[x][y] = [0, 0];
         }
     }
 
-    //Initialize MandelPlotter
-    this.mandelPlotter = new LaplaceEqSolver(this.canvas.width, this.canvas.height);
+    //Variables
+    this.bond = new Array();
 
-    //Reset...or start
-    this.resetCanvas();
+
+    //Initialize LES
+    this.LES = new LaplaceEqSolver(this.canvas.width, this.canvas.height);
+
+    //Set boundary and path
     this.setBoundary();
+    this.setPath(Math.floor(this.canvas.width/2), Math.floor(this.canvas.height/2));
     this.solveLEQ();
     this.updateCanvas();
+    this.ct.putImageData(this.imageData, 0, 0);
+    this.animatingNow = false;
+    this.startAnimation();
+}
+toggleAnimation(){
+    if(this.animatingNow){
+        this.stopAnimation();
+    }else{
+        this.startAnimation();
+    }
 }
 resizeCanvas(){
     this.canvas.width = Math.floor(window.innerWidth) - 20;
@@ -47,45 +66,173 @@ setBoundary(){
     const yCen = this.canvas.height/ 2;
     for(let x = 0; x < this.canvas.width; x++){
         for(let y = 0; y < this.canvas.height; y++){
-            if( (x - xCen) * (x - xCen) + (y - yCen) * (y - yCen) > xCen * xCen){
-                this.mandelPlotter.setBoundary(x, y, 1);
-                //this.potentialMap[x][y] = 1;
-            }else if( (x - xCen) == 0 && (y - yCen) == 0){
-                this.mandelPlotter.setBoundary(x, y, 0);
-                //this.potentialMap[x][y] = 0;
+            if( (x - xCen) * (x - xCen) + (y - yCen) * (y - yCen) > xCen * xCen * 0.99){
+                this.pixStatusMap[x][y] = 1;
+                this.potentialMap[x][y] = 1;
             }
         }
     }
 }
+setPath(x, y, xo = -1, yo){
+    if(xo == -1){
+        this.parentMap[x][y] = -1;
+    }else{
+        this.parentMap[x][y] = [xo, yo];
+    }
+    //Update Status and potential
+    this.pixStatusMap[x][y] = 2;
+    this.potentialMap[x][y] = 0;
+
+    // Check a neighbor and add bond if not invaded
+    if(this.pixStatusMap[x - 1][y    ] == 0){
+        this.bond[this.bond.length] = [[x, y], [x - 1, y    ]];
+    // Check a neighbor and remove bond if invaded
+    }else if(this.pixStatusMap[x - 1][y    ] == 2){
+        for(let bondIndex = 0; bondIndex < this.bond.length; bondIndex++){
+            if(this.bond[bondIndex][1][0] == (x - 1) &&
+               this.bond[bondIndex][1][1] == y){
+                this.bond.splice(bondIndex, 1);
+            }
+        }
+    }else if(this.pixStatusMap[x - 1][y    ] == 1){
+        console.log("STOP");
+        this.stopAnimation();
+        return;
+    }
+
+    // Check a neighbor and add bond if not invaded
+    if(this.pixStatusMap[x + 1][y    ] == 0){
+        this.bond[this.bond.length] = [[x, y], [x + 1, y    ]];
+    // Check a neighbor and remove bond if invaded
+    }else if(this.pixStatusMap[x + 1][y    ] == 2){
+        for(let bondIndex = 0; bondIndex < this.bond.length; bondIndex++){
+            if(this.bond[bondIndex][1][0] == (x + 1) &&
+               this.bond[bondIndex][1][1] == y){
+                this.bond.splice(bondIndex, 1);
+            }
+        }
+    }else if(this.pixStatusMap[x + 1][y    ] == 1){
+        console.log("STOP");
+        this.stopAnimation();
+        return;
+    }
+
+    // Check a neighbor and add bond if not invaded
+    if(this.pixStatusMap[x    ][y - 1] == 0){
+        this.bond[this.bond.length] = [[x, y], [x    , y - 1]];
+    // Check a neighbor and remove bond if invaded
+    }else if(this.pixStatusMap[x    ][y - 1] == 2){
+        for(let bondIndex = 0; bondIndex < this.bond.length; bondIndex++){
+            if(this.bond[bondIndex][1][0] == x &&
+               this.bond[bondIndex][1][1] == (y - 1)){
+                this.bond.splice(bondIndex, 1);
+            }
+        }
+    }else if(this.pixStatusMap[x    ][y - 1] == 1){
+        console.log("STOP");
+        this.stopAnimation();
+        return;
+    }
+
+    // Check a neighbor and add bond if not invaded
+    if(this.pixStatusMap[x    ][y + 1] == 0){
+        this.bond[this.bond.length] = [[x, y], [x    , y + 1]];
+    // Check a neighbor and remove bond if invaded
+    }else if(this.pixStatusMap[x    ][y + 1] == 2){
+        for(let bondIndex = 0; bondIndex < this.bond.length; bondIndex++){
+            if(this.bond[bondIndex][1][0] == x &&
+               this.bond[bondIndex][1][1] == (y + 1)){
+                this.bond.splice(bondIndex, 1);
+            }
+        }
+    }else if(this.pixStatusMap[x    ][y + 1] == 1){
+        console.log("STOP");
+        this.stopAnimation();
+        return;
+    }
+
+    //Remove bond to new pixel
+    for(let bondIndex = 0; bondIndex < this.bond.length; bondIndex++){
+        if(this.bond[bondIndex][1][0] == x &&
+           this.bond[bondIndex][1][1] == y){
+            this.bond.splice(bondIndex, 1);
+        }
+    }
+}
+findNextPath(){
+    let probabilitySum = 0;
+    for(let bondIndex = 0; bondIndex < this.bond.length; bondIndex++){
+        const bondX = this.bond[bondIndex][1][0];
+        const bondY = this.bond[bondIndex][1][1];
+        probabilitySum += this.potentialMap[bondX][bondY];
+    }
+    let rand = Math.random() * probabilitySum;
+    for(let bondIndex = 0; bondIndex < this.bond.length; bondIndex++){
+        const bondX = this.bond[bondIndex][1][0];
+        const bondY = this.bond[bondIndex][1][1];
+        rand -= this.potentialMap[bondX][bondY];
+        if(rand < 0){
+            //bondIndex at this moment is to be invaded next
+            this.setPath(bondX, bondY, this.bond[bondIndex][0][0], this.bond[bondIndex][0][1]);
+            return [bondX, bondY];
+        }
+    }
+}
 solveLEQ(){
-    this.mandelPlotter.updatePotentialField();
+    for(let i = 0; i < 1; i++){
+        this.potentialMap = this.LES.solveLaplaceEquation(this.potentialMap, this.pixStatusMap);
+   }
 }
 updateCanvas(){
     for(let x = 0; x < this.canvas.width; x++){
         for(let y = 0; y < this.canvas.height; y++){
             this.setPix([x, y], this.interpolate(this.potentialMap[x][y]));
+            //this.setPix([x, y], this.interpolate(this.pixStatusMap[x][y]));
         }
     }
-    this.ct.putImageData(this.imageData, 0, 0);
 }
-updateMandelBrotSet(xMin, yMin, xMax, yMax){
-    //get mandelbrot set
-    const startTime = new Date();
-    this.deathMap = this.mandelPlotter.updateMandelBrotSet(xMin, yMin, xMax, yMax);
-    console.log(new Date() - startTime + "ms");
+startAnimation(){
+    this.animatingNow = true;
+    let newXY;
+    for(let speed = 0; speed < 1; speed++){
+        this.solveLEQ();
+        newXY = this.findNextPath();
+    }
     this.updateCanvas();
+    this.paintNewPath(newXY);
+    this.ct.putImageData(this.imageData, 0, 0);
+    if(this.animatingNow) this.animation = requestAnimationFrame(() => {this.startAnimation();});
 }
-setPix(index, value){
+stopAnimation(){
+    if(!this.animatingNow) return;
+    cancelAnimationFrame(this.animation);
+    this.animatingNow = false;
+}
+paintNewPath(newXY){
+    this.setPix(newXY,255, 0);
+    while(this.parentMap[newXY[0]][newXY[1]] != -1){
+        newXY = this.parentMap[newXY[0]][newXY[1]];
+        this.setPix(newXY,255, 0);
+    }
+}
+setPix(index, value, type = -1){
     if(index.length == 2){
         index = this.xy2i(index);
     }
-    //Set the pixel based on type
-    this.imageData.data[4 * index + 0] = value;
-    this.imageData.data[4 * index + 1] = value;
-    this.imageData.data[4 * index + 2] = value;
+    if(type == -1){
+        //Set the pixel based on type
+        this.imageData.data[4 * index + 0] = value;
+        this.imageData.data[4 * index + 1] = value;
+        this.imageData.data[4 * index + 2] = value;
+    }else{
+        this.imageData.data[4 * index + type] = value;
+    }
 }
 interpolate(value){
-    return value * 255;
+    //return value * 256;
+    if(value == 0) return 0;
+    if(value == 1) return 255;
+    return value * 128 + 127;
 }
 i2xy(i){
     return [i % this.canvas.width, Math.floor(i / this.canvas.width)];
